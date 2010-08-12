27635154e722a3ad970c23d0d58b4a3cbad7e5e3
Kristian Mandrup <kmandrup@gmail.com>
2010-08-12 15:40:28 +0200

    added methods to check for multiple roles, any or all - also added 'drive_it' file to show how to use it - as described in README

--- lib/role_model/implementation.rb
+++ lib/role_model/implementation.rb
@@ -10,13 +10,21 @@ module RoleModel
     def roles
       Roles.new(self, self.class.valid_roles.reject { |r| ((self.send(self.class.roles_attribute_name) || 0) & 2**self.class.valid_roles.index(r)).zero? })
     end
-    alias role_symbols roles
+    alias_method :role_symbols, :roles
 
-    # check if a given role has been assigned
-    def has_role?(role)
-      roles.include?(role.to_sym)
+    # check if a given role has been assigned 
+    # if a list of roles: check if ALL of the given roles have been assigned 
+    def has_roles?(*roles)
+      (roles.to_a - roles.flatten).empty?      
     end
-    alias is? has_role?
-
+    alias_method :is?, :has_roles?
+    # alias_method :have_roles?, :has_roles?
+    
+    # check if any (at least ONE) of the given roles have been assigned
+    def has_role? *roles
+      !(roles.flatten & self.class.valid_roles).empty?            
+    end
+    alias_method :has?, :has_role?
+    # alias_method :have_role?, :has_role?
   end
 end
