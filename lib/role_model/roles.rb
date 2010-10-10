58e4e06bd12635cbfc049ebcce4239e217ca41ab
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2010-10-10 15:25:47 +0200

    support most Set methods on #roles
    add, merge, subtract are explicitly tested
    
    others like ^, |, &, - should work as well

--- lib/role_model/roles.rb
+++ lib/role_model/roles.rb
@@ -4,15 +4,17 @@ module RoleModel
   class Roles < ::Set # :nodoc:
     attr_reader :model_instance
 
-    def initialize(sender, *roles)
-      super(*roles)
+    def initialize(sender, roles)
+      super(roles)
       @model_instance = sender
     end
 
-    def <<(role)
-      model_instance.roles = super
+    def add(role)
+      roles = super
+      model_instance.roles = roles if model_instance
       self
     end
+    alias_method :<<, :add
 
     def delete(role)
       model_instance.roles = super(role.to_sym)
@@ -21,4 +23,3 @@ module RoleModel
 
   end
 end
-
--- spec/roles_spec.rb
+++ spec/roles_spec.rb
@@ -5,6 +5,9 @@ describe RoleModel::Roles do
   let(:model_instance) { Class.new.new }
   let(:array) { [:foo, :bar] }
   subject { RoleModel::Roles.new(model_instance, array) }
+  before(:each) do
+    model_instance.stub(:roles=)
+  end
 
   its(:model_instance) { should equal(model_instance) }
   it { should include(:foo, :bar) }
@@ -17,11 +20,33 @@ describe RoleModel::Roles do
     end
   end
 
+  describe "#add" do
+    it "should add the given element to the model_instance.roles by re-assigning all roles" do
+      model_instance.should_receive(:roles=).with(array_including(:foo, :bar, :baz))
+      subject.add(:baz)
+    end
+  end
+
+  describe "#merge" do
+    it "should add the given enum to the model_instance.roles by re-assigning all roles" do
+      model_instance.should_receive(:roles=).with(array_including(:foo, :bar, :baz, :quux))
+      subject.merge([:baz, :quux])
+    end
+  end
+
   describe "#delete" do
     it "should delete the given element to the model_instance.roles by re-assigning all roles" do
       model_instance.should_receive(:roles=).with(subject)
       subject.delete :foo
       subject.should_not include(:foo)
+    end
+  end
+
+  describe "#subtract" do
+    it "should remove the given enum to the model_instance.roles by re-assigning all roles" do
+      model_instance.should_receive(:roles=).with(subject)
+      subject.subtract([:foo, :bar])
+      subject.should have(0).roles
     end
   end
 end
